c82b07da41650b6d4e1a43a938690d0ef7b5fb78
shelbytrembly <112991855+shelbytrembly@users.noreply.github.com>
2022-12-12 22:49:50 -0500

    Added link
    I added the link to the index folder like the study guide said to do

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 const Index = () => {
   return (
     <div>
+      <Link to= "/about"> About Page</Link>
       <h1> My Landing Page</h1>
       <p> This is my landing page</p>
     </div> 
   )
 }
 export default Index
+
+
 
  
