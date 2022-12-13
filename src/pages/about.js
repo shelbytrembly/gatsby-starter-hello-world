b61bfde2a11e13eeb0260a604e7fa6a665fa62af
shelbytrembly <112991855+shelbytrembly@users.noreply.github.com>
2022-12-12 22:40:28 -0500

    About .js page
    I created the about page from the study guide image

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1> My About Page</h1>
+            <p> This is my about Page</p>
+        </div>
+    )
+    }
+    export default About
+    
