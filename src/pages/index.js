88af903fc533037b5f87e25b04cf60ed1a012d39
shelbytrembly <112991855+shelbytrembly@users.noreply.github.com>
2022-12-12 22:34:09 -0500

    UPDATE

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import * as React from "react"
 
-export default function Home() {
-  return <div>THIS MY NEW SITE</div>
+const Index = () => {
+  return (
+    <div>
+      <h1> My Landing Page</h1>
+      <p> This is my landing page</p>
+    </div> 
+  )
 }
+export default Index
+
+ 
